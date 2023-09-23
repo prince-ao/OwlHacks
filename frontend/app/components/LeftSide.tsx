8f7bc3773ff4b0c7f701d4a1d7f65198b3c1a186
David <davidliao9310@gmail.com>
2023-09-23 13:25:32 -0400

    fixing styling

--- frontend/app/components/LeftSide.tsx
+++ frontend/app/components/LeftSide.tsx
@@ -6,29 +6,29 @@ export default function LeftSide() {
     <div className="flex flex-col items-center mt-16">
       
   
-      <div className="hover:bg-blue-600 hover:cursor-pointer h-[75px] items-center w-[100%] flex justify-center">
+      <div className="hover:bg-blue-600 hover:cursor-pointer h-[75px] items-center w-[100%] flex justify-left ml-[10px]">
             <img className= "flex items-center justify-center h-[30px] w-[30px]" src="/compsciwhite.png" alt="Computer Science" />
-        <p className="text-3xl">Computer Science</p>
+        <p className="ml-[10px] text-2xl">Computer Science</p>
       </div>
      
-      <div className="hover:bg-blue-600 hover:cursor-pointer h-[75px] items-center w-[100%] flex justify-center">
+      <div className="hover:bg-blue-600 hover:cursor-pointer h-[75px] items-center w-[100%] flex justify-left ml-[10px]">
         <img className= "flex items-center justify-center h-[30px] w-[30px]" src="/englishwhite.png" alt="English" />
-        <p className="text-3xl">English</p>
+        <p className="ml-[10px] text-2xl">English</p>
       </div>
       
-      <div className="hover:bg-blue-600 hover:cursor-pointer h-[75px] items-center w-[100%] flex justify-center">
+      <div className="hover:bg-blue-600 hover:cursor-pointer h-[75px] items-center w-[100%] flex justify-left ml-[10px]">
        <img className="flex items-center justify-center h-[30px] w-[30px]" src="/physicswhite.png" alt="Physics" />
-        <p className="text-3xl">Physics</p>
+        <p className="ml-[10px] text-2xl">Physics</p>
       </div>
       
-      <div className="hover:bg-blue-600 hover:cursor-pointer h-[75px] items-center w-[100%] flex justify-center">
+      <div className="hover:bg-blue-600 hover:cursor-pointer h-[75px] items-center w-[100%] flex justify-left ml-[10px]">
       <img className="flex items-center justify-center h-[30px] w-[30px]" src="/biologywhite.png" alt="Biology" />
-        <p className="text-3xl">Biology</p>
+        <p className="ml-[10px] text-2xl">Biology</p>
       </div>
       
-      <div className="hover:bg-blue-600 hover:cursor-pointer h-[75px] items-center w-[100%] flex justify-center">
+      <div className="hover:bg-blue-600 hover:cursor-pointer h-[75px] items-center w-[100%] flex justify-left ml-[12px]">
       <img className = "flex items-center justify-center h-[30px] w-[30px]" src="/yourbookswhite.png" alt="Your Books" />
-        <p className="text-3xl">Your Books</p>
+        <p className="ml-[10px] text-2xl">Your Books</p>
       </div>
     </div>
   );

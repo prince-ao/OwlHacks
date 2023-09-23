58d8cffd78c1a95b45aebca023aa80231dcfc4cc
David <davidliao9310@gmail.com>
2023-09-23 13:52:18 -0400

    use state to shelfs

--- frontend/app/components/Body.tsx
+++ frontend/app/components/Body.tsx
@@ -1,14 +1,19 @@
+"use client"
+
+import { useState } from "react";
 import LeftSide from "./LeftSide";
 import Shelf from "./Shelf";
 
 export default function Body() {
+  const [shelfName, setShelfName] = useState("");
+
   return (
     <div className="flex">
       <div className="w-[20%] min-h-page border-e-2">
-        <LeftSide />
+        <LeftSide setShelfName={setShelfName} />
       </div>
       <div>
-        <Shelf shelf_name={"somethinmg"} />
+        <Shelf shelf_name={shelfName} />
       </div>
     </div>
   );
--- frontend/app/components/LeftSide.tsx
+++ frontend/app/components/LeftSide.tsx
@@ -1,33 +1,41 @@
-import React from "react";
+import {Dispatch,SetStateAction } from "react";
 
-
-export default function LeftSide() {
+export default function LeftSide({ setShelfName }: {setShelfName: Dispatch<SetStateAction<string>>}) {
   return (
     <div className="flex flex-col items-center mt-16">
-      
-  
-      <div className="hover:bg-blue-600 hover:cursor-pointer h-[75px] items-center w-[100%] flex justify-left ml-[10px]">
-            <img className= "flex items-center justify-center h-[30px] w-[30px]" src="/compsciwhite.png" alt="Computer Science" />
+      <div 
+        className="hover:bg-blue-600 hover:cursor-pointer h-[75px] items-center w-[100%] flex justify-left ml-[10px]"
+        onClick={() => setShelfName("Computer Science")}
+      >
+        <img className="flex items-center justify-center h-[30px] w-[30px]" src="/compsciwhite.png" alt="Computer Science" />
         <p className="ml-[10px] text-2xl">Computer Science</p>
       </div>
-     
-      <div className="hover:bg-blue-600 hover:cursor-pointer h-[75px] items-center w-[100%] flex justify-left ml-[10px]">
-        <img className= "flex items-center justify-center h-[30px] w-[30px]" src="/englishwhite.png" alt="English" />
+      <div 
+        className="hover:bg-blue-600 hover:cursor-pointer h-[75px] items-center w-[100%] flex justify-left ml-[10px]"
+        onClick={() => setShelfName("English")}
+      >
+        <img className="flex items-center justify-center h-[30px] w-[30px]" src="/englishwhite.png" alt="English" />
         <p className="ml-[10px] text-2xl">English</p>
       </div>
-      
-      <div className="hover:bg-blue-600 hover:cursor-pointer h-[75px] items-center w-[100%] flex justify-left ml-[10px]">
-       <img className="flex items-center justify-center h-[30px] w-[30px]" src="/physicswhite.png" alt="Physics" />
+      <div 
+        className="hover:bg-blue-600 hover:cursor-pointer h-[75px] items-center w-[100%] flex justify-left ml-[10px]"
+        onClick={() => setShelfName("Physics")}
+      >
+        <img className="flex items-center justify-center h-[30px] w-[30px]" src="/physicswhite.png" alt="Physics" />
         <p className="ml-[10px] text-2xl">Physics</p>
       </div>
-      
-      <div className="hover:bg-blue-600 hover:cursor-pointer h-[75px] items-center w-[100%] flex justify-left ml-[10px]">
-      <img className="flex items-center justify-center h-[30px] w-[30px]" src="/biologywhite.png" alt="Biology" />
+      <div 
+        className="hover:bg-blue-600 hover:cursor-pointer h-[75px] items-center w-[100%] flex justify-left ml-[10px]"
+        onClick={() => setShelfName("Biology")}
+      >
+        <img className="flex items-center justify-center h-[30px] w-[30px]" src="/biologywhite.png" alt="Biology" />
         <p className="ml-[10px] text-2xl">Biology</p>
       </div>
-      
-      <div className="hover:bg-blue-600 hover:cursor-pointer h-[75px] items-center w-[100%] flex justify-left ml-[12px]">
-      <img className = "flex items-center justify-center h-[30px] w-[30px]" src="/yourbookswhite.png" alt="Your Books" />
+      <div 
+        className="hover:bg-blue-600 hover:cursor-pointer h-[75px] items-center w-[100%] flex justify-left ml-[12px]"
+        onClick={() => setShelfName("Your Books")}
+      >
+        <img className="flex items-center justify-center h-[30px] w-[30px]" src="/yourbookswhite.png" alt="Your Books" />
         <p className="ml-[10px] text-2xl">Your Books</p>
       </div>
     </div>

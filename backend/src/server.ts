c5389221e8c427c552373d277f81b6c611ec2846
David <davidliao9310@gmail.com>
2023-09-24 04:59:11 -0400

    implimented  redis to cache user info

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,10 +2,33 @@ import express from "express";
 import cors from "cors";
 import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
 import pdf from "pdf-parse";
+import { createClient } from 'redis';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 const app = express();
 const PORT = 3005;
-const client = new S3Client({ region: "us-east-2" });
+const s3Client = new S3Client({ region: "us-east-2" });
+
+// Using redis to connect with Auth0 to cache user data for more centralized database access.
+const redisClient = createClient({
+    password: process.env.REDIS_CLOUD_PASSWORD,
+    socket: {
+        host: process.env.REDIS_CLOUD_URL,
+        port: process.env.REDIS_CLOUD_PORT
+    }
+});
+
+// Add a new route to get user data
+app.post('/api/get-user-data', (req, res) => {
+  const userId = req.body.userId;
+  redisClient.get('user_data:' + userId, (err, reply) => {
+    const userData = JSON.parse(reply);
+    res.send(userData);
+  });
+});
 
 app.use(cors());
 
@@ -15,7 +38,7 @@ app.get("/get-pdf", async (req, res) => {
     Bucket: `owlhacks`,
     Key: `pdfs/${category}/${book}`,
   });
-  const response = await client.send(command);
+  const response = await s3Client.send(command);
   const arrayResponse = await response.Body?.transformToByteArray();
   const pdfData = await pdf(arrayResponse as Buffer);
 

1a97c4f705b9c8e8d7da72c7af7642da364b0c28
prince-ao <prince.addo@cix.csi.cuny.edu>
2023-09-23 21:06:36 -0400

    some stuff

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,26 +1,44 @@
-"use client"
+"use client";
 
-import { useRouter } from 'next/navigation'
-import Header from "./components/Header";
-import Body from "./components/Body";
-import Footer from "./components/Footer";
-import TextToSpeech from "./components/TextToSpeech";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { Rowdies } from "next/font/google";
+
+const rowdies = Rowdies({ weight: "400", subsets: ["latin"] });
 
 export default function Home() {
-    const router = useRouter()
+  const router = useRouter();
   return (
-    <div className="landing-page">
-      <header>
-        <h1>Welcome to Our Landing Page</h1>
+    <div className="min-h-screen flex flex-col justify-center items-center bg-[#401654]">
+      <header className="text-center mb-8">
+        <Image
+          src="/bookstack.png"
+          alt="Your Logo"
+          className="mb-2 mx-auto"
+          width={200}
+          height={200}
+        />
+        <h1 className="text-4xl font-bold">
+          Welcome to <span className={rowdies.className}>Bookstack.ai</span>
+        </h1>
       </header>
-      <main>
-        <a href="/api/auth/login">login</a>
-        <p>This is a simple landing page created with TypeScript and React.</p>
-        <button>Get Started</button>
-      </main>
-      <footer>
-        <p>&copy; 2023 Your Company</p>
-      </footer>
+      <div className="text-lg text-gray-600 text-center mb-16 flex justify-center">
+        <p className="text-white w-[52%]">
+          Welcome to a world where the power of literature is accessible to
+          everyone. Our mission goes beyond the mere digitization of books -
+          we're bridging the gap between stories and their readers.
+        </p>
+      </div>
+      <div className="flex gap-4">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <a href="/api/auth/login">Log in</a>
+        </button>
+        <button className="bg-gray-100 hover:bg-gray-400 text-white font-bold py-2 px-4 rounded">
+          <a href="/api/auth/login" className="text-black">
+            Sign up
+          </a>
+        </button>
+      </div>
     </div>
   );
 }

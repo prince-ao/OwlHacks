d5b9bf07ef1862210a30fc54aa569ee9c73f8d79
David <davidliao9310@gmail.com>
2023-09-24 04:38:51 -0400

    attempting to make tts work on firefox + speed

--- frontend/app/components/TextToSpeech.tsx
+++ frontend/app/components/TextToSpeech.tsx
@@ -1,31 +1,26 @@
-"use client";
-
 import React from "react";
 import { Button } from "@/components/ui/button";
 import { Speech } from "lucide-react";
 
 const TextToSpeech = ({ page }: { page?: string }) => {
-  const handleButtonClick = () => {
-    const text = window.getSelection()?.toString();
-    if (text) {
-      const _speechSynth = window.speechSynthesis;
-      const voices = _speechSynth.getVoices();
-      console.log(voices);
-      const utterance = new SpeechSynthesisUtterance(text);
-      window.speechSynthesis.speak(utterance);
-    } else {
-      const utterance = new SpeechSynthesisUtterance(page);
-      window.speechSynthesis.speak(utterance);
-    }
-  };
+    const handleButtonClick = () => {
+        const text = window.getSelection()?.toString() || page;
+        const utterance = new SpeechSynthesisUtterance(text);
+        utterance.rate = 0.8; // Set the speech rate to a slower value
+        window.speechSynthesis.onvoiceschanged = () => {
+            const voices = window.speechSynthesis.getVoices();
+            utterance.voice = voices[0]; // Set the voice for the utterance
+            window.speechSynthesis.speak(utterance);
+        };
+    };
 
-  return (
-    <div>
-      <Button variant="outline" size="icon" onClick={handleButtonClick}>
-        <Speech className="h-[1.2rem] w-[1.2rem]" />
-      </Button>
-    </div>
-  );
+    return (
+        <div>
+            <Button variant="outline" size="icon" onClick={handleButtonClick}>
+                <Speech className="h-[1.2rem] w-[1.2rem]" />
+            </Button>
+        </div>
+    );
 };
 
 export default TextToSpeech;
